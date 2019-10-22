aa0cb3f4564d0bcb987f921d2760e6fbcd262088
Tobbe3108 <Tobbe3108@gmail.com>
2019-10-22 21:22:17 +0200

    Update page.vue

--- src/components/page.vue
+++ src/components/page.vue
@@ -1,40 +1,9 @@
-<template>
-  <div class="slider">
-    <div>Slide #1</div>
-    <div>Slide #2</div>
-    <div>Slide #3</div>
-    <div>Slide #4</div>
-    <div>Slide #5</div>
-  </div>
-</template>
+<template></template>
 
 <script>
-export default {
-  name: "page"
-};
+export default {};
 </script>
 
 <style>
-.slider {
-  width: 100%;
-  text-align: center;
-  overflow: hidden;
-}
-.slider {
-  display: flex;
-
-  overflow-x: auto;
-  scroll-snap-type: x mandatory;
-
-  scroll-behavior: smooth;
-  -webkit-overflow-scrolling: touch;
-}
-.slider > div {
-  scroll-snap-align: start;
-  flex-shrink: 0;
-  width: 100%;
-  height: 300px;
-  background: #eee;
-}
 </style>
 
